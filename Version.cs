8ade859d2f22677dcd6fa52329e919145a06c731
tom-englert <mail@tom-englert.de>
2017-10-21 15:42:52 +0200

    V 1.0

--- Version.cs
+++ Version.cs
@@ -2,10 +2,9 @@ using System.Reflection;
 
 [assembly: AssemblyVersion(Product.Version)]
 [assembly: AssemblyFileVersion(Product.Version)]
-[assembly: AssemblyInformationalVersion("0.9.2.0-beta")]
 
 internal static class Product
 {
-    public const string Version = "0.9.2.0";
+    public const string Version = "1.0.0.0";
 }
 
